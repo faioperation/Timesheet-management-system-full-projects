bf9fcd05cf9df3991c30acbe27e66c64c7754b4e
Kishor Mahmud <mahmudkishor9@gmail.com>
2026-07-08 15:32:58 +0600

    add app code

--- flutter-app-code/lib/app/session_manager.dart
+++
@@ -1,1 +0,0 @@
-// JWT token, role management
--- flutter-app-code/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:timesheet_naresh/myApp.dart';
-
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const Myapp());
-}
